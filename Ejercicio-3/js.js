704800ccf47d7897a5381ccc11e870e54f04c264
Eugenia <75372203+eugerbasile@users.noreply.github.com>
2021-09-16 13:12:36 +0200

    Update js.js

--- Ejercicio-3/js.js
+++ Ejercicio-3/js.js
@@ -1,10 +1,41 @@
-var cadenaNombre = [{ E: 2 , U: 1, G: 1,  N: 1 ,  I: 1 , A: 1 }];
-//metodo map crea un nuevo array con los resultados de una llamada a una funcion
-var numeroVeces = cadenaNombre.map(function (current) {
-    console.log(current);
-    
+/*al utilizar map(), podremos convertir un array en uno nuevo, cuyos valores serán una transformación del primero. Dicha transformación será efectuada por la función que se utilice en este proceso.*/
+
+/*function contarCaracteres(letras) {
+    var nombre = ["E","U", "G","E", "N", "I", "A"];
+    var contador = 0;
+  
+        contador = contador + 1;
+        
+    }
+  }
+  console.log(contarCaracteres  );*/
+
+/*var nombre = ["E","U", "G","E", "N", "I", "A"];
+    var contador = {};
+
+    var contarCaracteres = nombre.map (function(){
+
+        contador[contarCaracteres] = ;
+        console.log(nombre[i] + contarCaracteres.length);
+
+    });
+  */
+
+var nombre = ["E", "U", "G", "E", "N", "I", "A"];
+var listedCounts = {};
+//var count;
+
+var nameMaped = nombre.map((letter) => {
+  console.log(letter);
+  if (!listedCounts[letter]) {
+    listedCounts[letter] = 1;
+  } else {
+    listedCounts[letter]++;
+  }
+  return listedCounts;
 });
 
+console.log(listedCounts);
 
 /*la funcion del map nos pide tres parametros
 current(es el recorrido de la funcion map)
